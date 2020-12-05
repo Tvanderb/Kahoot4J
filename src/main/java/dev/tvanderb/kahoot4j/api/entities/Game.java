d5d8d67df8b75b35973f21e45e7abdc5dbdb9774
Talon Vanderbeken <talon.vanderbeken@gmail.com>
2020-12-05 14:43:56 -0500

    Added javadoc to Game.java

--- src/main/java/dev/tvanderb/kahoot4j/api/entities/Game.java
+++ src/main/java/dev/tvanderb/kahoot4j/api/entities/Game.java
@@ -13,16 +13,34 @@ public interface Game {
         QUIZ;
     }
 
+    /**
+     * @return The {@link KahootClient KahootClient} that is in this game.
+     */
     KahootClient getSelf();
 
+    /**
+     * @return The name of the game.
+     */
     String getName();
 
+    /**
+     * @return The total amount of points the {@link KahootClient KahootClient} has.
+     */
     int getPoints();
 
+    /**
+     * @return The game pin of this {@link Game}.
+     */
     int getGamePin();
 
+    /**
+     * @return The number of questions in this game.
+     */
     int getQuestionCount();
 
+    /**
+     * Disconnect from this game.
+     */
     void disconnect();
 
 }
