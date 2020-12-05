f806b2107090dcf6babf47aaaec0b0260b9bd5af
Talon Vanderbeken <talon.vanderbeken@gmail.com>
2020-12-05 11:49:10 -0500

    Create DefaultEventManager.java and updated KahootEventListener.
    Created a method in KahootEventListener to accept all events. It then takes that event and provides it to any annotated EventHandlers inside of itself.

---
+++ src/main/java/dev/tvanderb/kahoot4j/api/events/DefaultEventManager.java
@@ -0,0 +1,37 @@
+package dev.tvanderb.kahoot4j.api.events;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DefaultEventManager implements IEventManager {
+
+    private final List<KahootEventListener> eventListeners = new ArrayList<>();
+
+    @Override
+    public void addEventListener(@NotNull KahootEventListener listener) {
+        eventListeners.add(listener);
+    }
+
+    @Override
+    public void removeEventListener(@NotNull KahootEventListener listener) {
+        eventListeners.remove(listener);
+    }
+
+    @Override
+    public @NotNull List<KahootEventListener> getEventListeners() {
+        return eventListeners;
+    }
+
+    @Override
+    public void sendEvent(@NotNull Event event) {
+        for (KahootEventListener listener : eventListeners) {
+            try {
+                listener.accept(event);
+            } catch (Exception ignore) {}
+        }
+    }
+
+}
--- src/main/java/dev/tvanderb/kahoot4j/api/events/KahootEventListener.java
+++ src/main/java/dev/tvanderb/kahoot4j/api/events/KahootEventListener.java
@@ -1,8 +1,30 @@
 package dev.tvanderb.kahoot4j.api.events;
 
+import dev.tvanderb.kahoot4j.annotations.EventHandler;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * Interface to specify an event listener class.
  *
  * @author Talon 'tvanderb' Vanderbeken
  */
-public interface KahootEventListener {}
+public interface KahootEventListener {
+
+    default void accept(@NotNull Event e) {
+        for (final Method method : this.getClass().getDeclaredMethods()) {
+            if (method.isAnnotationPresent(EventHandler.class)) {
+                EventHandler handler = method.getAnnotation(EventHandler.class);
+
+                if (e.getClass().isInstance(handler.target())) {
+                    try {
+                        method.invoke(e);
+                    } catch (IllegalAccessException | InvocationTargetException ignore) {}
+                }
+            }
+        }
+    }
+
+}
