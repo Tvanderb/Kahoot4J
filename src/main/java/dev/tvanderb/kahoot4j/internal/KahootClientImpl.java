45a34e84d716fd47a638d6273118a8001003cb7e
Talon Vanderbeken <talon.vanderbeken@gmail.com>
2020-12-05 01:11:42 -0500

    Created a KahootClient implementation.

---
+++ src/main/java/dev/tvanderb/kahoot4j/internal/KahootClientImpl.java
@@ -0,0 +1,38 @@
+package dev.tvanderb.kahoot4j.internal;
+
+import dev.tvanderb.kahoot4j.api.KahootClient;
+
+/**
+ * Implementation of {@link dev.tvanderb.kahoot4j.api.KahootClient KahootClient}.
+ *
+ * @author Talon 'tvanderb' Vanderbeken
+ */
+public class KahootClientImpl implements KahootClient {
+
+    private ClientStatus status = ClientStatus.PRE_INITIALIZATION;
+
+    private String username;
+
+    /**
+     * Create a new default {@link dev.tvanderb.kahoot4j.internal.KahootClientImpl KahootClient} implementation.
+     */
+    public KahootClientImpl(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public ClientStatus getCurrentStatus() {
+        return status;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
