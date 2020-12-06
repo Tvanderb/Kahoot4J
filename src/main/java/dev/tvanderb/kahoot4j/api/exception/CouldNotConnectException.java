be563b18dc37898e96c1878be2dd98bd97569a38
Talon Vanderbeken <talon.vanderbeken@gmail.com>
2020-12-06 01:58:42 -0500

    Passed message into super constructor instead of providing it in the instance of CouldNotConnectException.

--- src/main/java/dev/tvanderb/kahoot4j/api/exception/CouldNotConnectException.java
+++ src/main/java/dev/tvanderb/kahoot4j/api/exception/CouldNotConnectException.java
@@ -2,24 +2,31 @@ package dev.tvanderb.kahoot4j.api.exception;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Thrown when a {@link dev.tvanderb.kahoot4j.api.KahootClient KahootClient} couldn't connect to a game.
+ */
 public class CouldNotConnectException extends Exception {
 
     private final Integer gamePin;
-    private final String message;
 
+    /**
+     * Create a new {@link CouldNotConnectException CouldNotConnectException} instance.
+     *
+     * @param gamePin The game pin that couldn't be connected to.
+     * @param message The message to display in the stack trace.
+     */
     public CouldNotConnectException(@NotNull Integer gamePin, @NotNull String message) {
+        super(message);
+
         this.gamePin = gamePin;
-        this.message = message;
     }
 
+    /**
+     * @return The game pin that couldn't be connected to.
+     */
     @NotNull
     public Integer getGamePin() {
         return gamePin;
-    }
-
-    @NotNull
-    public String message() {
-        return message;
     }
 
 }
